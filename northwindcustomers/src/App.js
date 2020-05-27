c13f4203d14556ba4e29af5b762513a8746caf26
jpickard393 <jpickard@and.digital>
2020-05-27 22:21:34 +0100

    Added empty component

--- northwindcustomers/src/App.js
+++ northwindcustomers/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CustomerList from './Components/CustomersList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <CustomerList></CustomerList>
     </div>
   );
 }
---
+++ northwindcustomers/src/Components/CustomersList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CustomerList = () =>{
+    return (
+        <div>
+            <h1>Customers</h1>
+        </div>
+    )
+}
+export default CustomerList;
