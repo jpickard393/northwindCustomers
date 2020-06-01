9172333a2b60c7b3696ad71ffe2149af754edf2a
jpickard393 <jpickard@and.digital>
2020-06-01 14:04:22 +0100

    Now gets dta from API

--- northwindcustomers/src/App.js
+++ northwindcustomers/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CustomerList from './Components/CustomersList';
+//import CustomerListClass from './Components/CustomerListClass';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <CustomerList></CustomerList>
+      {/* <CustomerListClass></CustomerListClass> */}
     </div>
   );
 }
--- northwindcustomers/src/Components/CustomersList.js
+++ northwindcustomers/src/Components/CustomersList.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './CustomerTableStyle.css';
 
 const CustomerList = () =>{
+    const [dataList,setDataList] = useState([]);
+
+    useEffect(() => {
+        let uri = 'https://localhost:44380/api/customer/get';
+        const resultSpan = document.getElementById('resultSpan');
+
+         // get the data from the api, and store it in the dataList array of state.
+        fetch(uri, {
+            mode: 'cors',
+            headers: {
+              'Access-Control-Allow-Origin':'*'
+                }
+            })
+            .then(response => response.json())
+            .then(data => setDataList(data)
+            )
+            .catch(error => resultSpan.innerText = error)
+    },[]);
+
+    const customers = dataList.map(
+        function(item){
+            let row = <tr><td>{item.id}</td><td>{item.companyName}</td></tr>;
+            return row
+        });
+    
     return (
         <div>
-            <h1>Customers</h1>
-        </div>
-    )
+                <table>
+                    <tr><th><td>Id</td></th><th><td>Company</td></th></tr>
+                    {customers}
+                </table>
+                <div id='resultSpan'>
+                    
+                </div>
+            </div>
+    );
+    
+
 }
+
+
+
 export default CustomerList;
